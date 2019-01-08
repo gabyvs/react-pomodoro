d7477bea98524189ed125bb3a28e8734a5c3dd1a
Gaby Vazquez <gabs.vz@gmail.com>
2019-01-07 21:54:36 -0800

    Updates

---
+++ precache-manifest.5b5cc6f6080f1d9494ef702ad8533486.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d44c52309990baf2f61",
+    "url": "/react-pomodoro/static/js/runtime~main.3d44c523.js"
+  },
+  {
+    "revision": "d015797fdc7b0f837bc2",
+    "url": "/react-pomodoro/static/js/main.d015797f.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/react-pomodoro/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "d015797fdc7b0f837bc2",
+    "url": "/react-pomodoro/static/css/main.856484cb.chunk.css"
+  },
+  {
+    "revision": "a0ab6fb4c0ee81157f0f4056f989426f",
+    "url": "/react-pomodoro/index.html"
+  }
+];
